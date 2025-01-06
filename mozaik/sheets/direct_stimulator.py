Add morphology rotation and fix bugs

mozaik/sheets/direct_stimulator.py
@@ -898,6 +898,10 @@ class OpticalStimulatorArrayMorphologyChR(DirectStimulator):
                      depth of the network point neurons as originally defined in 
                      sheet.pop.positions[2] is ignored.
 
+    morphology_rotation_angles : array-like
+                     Rotation of morphology per point neuron. Should be random to avoid
+                     non-realistic symmetry across neurons.
+
     Notes
     -----
 
@@ -914,7 +918,8 @@ class OpticalStimulatorArrayMorphologyChR(DirectStimulator):
             'transfection_proportion' : float,
             'neuron_node_data_path': str,
             'neuron_comp_data_path': str,
-            'soma_depth': int
+            'soma_depth': int,
+            'morphology_rotation_angles': np.ndarray
     })
 
     def __init__(self, sheet,parameters,shared_scs=None,optimized_scs=True):
@@ -952,9 +957,11 @@ class OpticalStimulatorArrayMorphologyChR(DirectStimulator):
             point_neuron_positions=np.array([
                 x_neurons, y_neurons, z_neurons
             ]),
+            rotation_angles=self.parameters.morphology_rotation_angles,
             neuron_node_data_path=self.parameters.neuron_node_data_path,
             neuron_comp_data_path=self.parameters.neuron_comp_data_path
         )
+        print('RONs loaded.')
         # calculate light fluxes and extract their time evolution from self.stimulator_signals
         RONs.calc_light_fluxes(
                 func_xyz_to_flux=self.light_flux_lookup_photonsPERfsPERcm2,
@@ -962,6 +969,7 @@ class OpticalStimulatorArrayMorphologyChR(DirectStimulator):
                                max_depth=self.sheet.parameters.max_depth,
                                depth_sampling_step=self.parameters.depth_sampling_step)
                 )
+        print('RONs fluxes calculated.')
         self.stimulation_duration = numpy.shape(RONs.light_fluxes_over_time)[1] * self.parameters.update_interval
         self.times = numpy.arange(0,self.stimulation_duration,self.parameters.update_interval)
 